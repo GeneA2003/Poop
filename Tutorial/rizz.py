4fc9bd1133a819459be9022fb31b945d77b5a1f6
GeneA2003 <gene.angeles123@gmail.com>
2024-07-29 18:48:48 -0400

    First ever commit

---
+++ Tutorial/rizz.py
@@ -0,0 +1,12 @@
+print("Hellow, World")
+
+name = "KL" 
+age = 50
+is_student = True
+
+print("Name: ", name)
+print("Age: ", age)
+print("Is student: ", is_student)
+
+# Team 
+team = ["Moussa", "Gene", "Reghan", "KL"]
